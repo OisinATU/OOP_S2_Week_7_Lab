4833cdabfc04ab513d26c5789080bfcf4f744f2f
OisinLoftus-STUDENT <G00470518@atu.ie>
2026-03-18 16:51:29 +0000

    Created Project

---
+++ src/main/java/ie/atu/oop_week7_lab/OopWeek7LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.oop_week7_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopWeek7LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopWeek7LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oop_week7_lab/OopWeek7LabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oop_week7_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopWeek7LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
